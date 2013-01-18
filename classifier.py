0e01f83b68ac7061818071a99d4c4fd4d18cd5b8
Murad Salahi <murad@scripted.com>
2013-01-17 16:15:22 -0800

    making things prettier

--- classifier.py
+++ classifier.py
@@ -7,19 +7,16 @@ import os
 
 
 def vec2dense(vec, num_terms):
-
-    '''
-    Helper function to convert sparse Gensim format vectors
-    into lists of numbers for display purposes.
-    '''
+    
+    '''Convert from sparse gensim format to dense list of numbers'''
     return list(gensim.matutils.corpus2dense([vec], num_terms=num_terms).T[0])
 
 if __name__ == '__main__':
 
     #Load in corpus, remove newlines, make strings lower-case
+
     docs = {}
     corpus_dir = 'corpus'
-
     for filename in os.listdir(corpus_dir):
 
         path = os.path.join(corpus_dir, filename)
@@ -30,9 +27,9 @@ if __name__ == '__main__':
 
     #Remove stopwords and split on spaces
     print "\n---Corpus with Stopwords Removed---"
-    preprocessed_docs = {}
-    stop = ['the', 'of', 'a', 'at', 'is']
 
+    stop = ['the', 'of', 'a', 'at', 'is']
+    preprocessed_docs = {}
     for name in names:
 
         text = docs[name].split()
@@ -55,22 +52,19 @@ if __name__ == '__main__':
 
     #Build Bag of Words Vectors out of preprocessed corpus
     print "---Bag of Words Corpus---"
-    bow_docs = {}
 
+    bow_docs = {}
     for name in names:
 
-        #Express our docs as BoW vectors
         sparse = dct.doc2bow(preprocessed_docs[name])
         bow_docs[name] = sparse
         dense = vec2dense(sparse, num_terms=len(dct))
         print name, ":", dense
 
-    print '\n'
-
     #Dimensionality reduction using LSI. Go from 6D to 2D.
-    print "---LSI Model---"
-    lsi_docs = {}
+    print "\n---LSI Model---"
 
+    lsi_docs = {}
     num_topics = 2
     lsi_model = gensim.models.LsiModel(bow_docs.values(), num_topics=num_topics)
     for name in names:
@@ -81,10 +75,10 @@ if __name__ == '__main__':
         lsi_docs[name] = sparse
         print name, ':', dense
 
-    print '\n'
-    print "---Unit Vectorization---"
-    unit_vecs = {}
+    #Normalize LSI vectors by setting each vector to unit length
+    print "\n---Unit Vectorization---"
 
+    unit_vecs = {}
     for name in names:
 
         vec = vec2dense(lsi_docs[name], num_topics)
@@ -93,10 +87,8 @@ if __name__ == '__main__':
         unit_vecs[name] = unit_vec
         print name, ':', unit_vec
 
-    print '\n'
-
     #Take cosine distances between docs and show best matches
-    print "---Document Similarities---"
+    print "\n---Document Similarities---"
 
     index = gensim.similarities.MatrixSimilarity(lsi_docs.values())
     for i, name in enumerate(names):
@@ -117,8 +109,9 @@ if __name__ == '__main__':
         match = names[match]
         print name, "is most similar to...", match
 
-    print '\n'
-    print "---Classification---"
+    #We add classes to the mix by labelling dog1.txt and sandwich1.txt
+    #We use these as our training set, and test on all documents.
+    print "\n---Classification---"
 
     dog1 = unit_vecs['dog1.txt']
     sandwich1 = unit_vecs['sandwich1.txt']
